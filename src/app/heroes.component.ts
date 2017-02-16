4cfb51cc7b6d4b63b332461ea62204e244b28afb
will-beta <github@will-beta.info>
2017-02-16 16:55:26 +0800

    first

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+import { Router } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: ['heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService
+  ) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
+}
